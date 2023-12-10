b50fa072352f3e41941eb78d7a4c6230abd54e47
Stifano <mateusz.fudzsl@hotmail.com>
2023-12-10 18:59:58 +0100

    Restored main function so no merge conflict

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from Core.Speciman import Specimen
 from Core.Chromosome import Chromosome
 from Core.FitnessFunction import FitnessFunction
 from Core.Utils import initRandomPopulation
-from Elitism.Elitism import Elitism
 
 
 def functionToCalculate():
@@ -19,10 +18,15 @@ if __name__ == '__main__':
     evolutionManager = EvolutionManager(10, 10, fitnessFunction)
     evolutionManager.setFirstPopulation(initRandomPopulation(10, 7, fitnessFunction))
 
-    evolutionSnapshot = evolutionManager.getEpochSnapshot()
-    currentPopulation = evolutionSnapshot.currentPopulation
-    for specimenInLoop in currentPopulation:
-        specimenInLoop.setSpecimenValue(evolutionSnapshot.fitnessFuntion.calculateValue(specimenInLoop))
+    evoSnapshot = evolutionManager.getEpochSnapshot()
 
-    Elitism.selectEliteSpecimensPercent(0.41, False, evolutionManager)
+    evolutionManager.updateSelectedPopulation([evoSnapshot.currentPopulation[0], evoSnapshot.currentPopulation[1]])
 
+    arr = [x for x in evoSnapshot.currentPopulation]
+    arr.extend([evoSnapshot.currentPopulation[1]])
+    evolutionManager.updateNewPopulation(arr)
+
+    for i in range(11):
+        evolutionManager.updateEpoch()
+
+    print(evolutionManager.getEpochSnapshot())
