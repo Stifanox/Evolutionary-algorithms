bf79c2c3820182a6668eee3c05558ed05c85686e
PPF_LAB2 <jakubmak81@gmail.com>
2024-01-01 19:47:09 +0100

    Fixed bug stopping program when specimen value was below zero. Added support for type of evaluation of specimens (minimum best, maximum best)

--- Selection/Selection.py
+++ Selection/Selection.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     #personal testing purposes
     class Specimen:
         def __init__(self):
-            self.val = random.randint(0,100)
+            self.val = round(random.uniform(-20,20)*100)/100
     class FitnessFunction:
         def calculateValue(self, specimen: Specimen) -> float:
             return specimen.val
@@ -24,18 +24,41 @@ class Selection():
     """
     Class managing selection of population
     """
-    LimitType_Constant = 1      #LimitValue=10 == 10 top osobnikow
-    LimitType_Percentage = 2    #LimitValue=10 == 10% top osobnikow
     
     SelectionType_Top = 1   #Top - we choose n amount of best specimens
     SelectionType_Roulette = 2    #Roulette - random, where fitness is a weight
     SelectionType_Tournament = 3    #Tournament - specimens fight each other in brackets untill there are more then desired specimens
 
-    def __init__(self, fitnessFunction: FitnessFunction, selectionType: int, selectionLimitType: str, selectionLimitValue: int):
+    LimitType_Constant = 1      #LimitValue=10 == 10 top osobnikow
+    LimitType_Percentage = 2    #LimitValue=10 == 10% top osobnikow
+
+    EvalMethod_Minimum = 1      #closer to zero the specimen fitness, the better
+    EvalMethod_Maximum = 2    #higher the specimen fitness, the better
+
+    def __init__(self, fitnessFunction: FitnessFunction, selectionType: int, selectionLimitType: str, selectionLimitValue: int, selectionEvalMethod: int):
+        """
+        Constructor for Selection class
+
+        :param fitnessFunction: Function to evaluate specimen fitness.
+        :type fitnessFunction: FitnessFunction
+
+        :param selectionType: Type of selection (Top,Roulette,Tournament).
+        :type selectionType: int
+
+        :param selectionLimitType: Type of final amount of selected specimens (Constant,Percentage).
+        :type selectionLimitType: int
+
+        :param selectionLimitValue: Value of final amount of selected specimens (Constant,Percentage).
+        :type selectionLimitValue: int
+
+        :param selectionEvalMethod: Method of evaluating specimens fitness (Minimum,Maximum).
+        :type selectionEvalMethod: int
+        """
         self.__fitnessFunction = fitnessFunction
         self.__selectionType = selectionType
         self.__selectionLimitType = selectionLimitType
         self.__selectionLimitValue = selectionLimitValue
+        self.__selectionEvalMethod = selectionEvalMethod
         
         self.__specimens_selection_amount = 0
 
@@ -49,8 +72,18 @@ class Selection():
         :return: Selected top specimens.
         :rtype: Collection[Specimen]
         """
-        specimens_dict = [{'val': self.__fitnessFunction.calculateValue(specimen), 'specimen': specimen} for specimen in specimens] #fitness values
-        
+        specimens_dict = [{'val': self.__fitnessFunction.calculateValue(specimen), 'specimen': specimen} for specimen in specimens] #fitness values        
+
+        if self.__selectionEvalMethod == Selection.EvalMethod_Minimum: #normalizing all values to be closest to minimum for MINIMUM
+            specimens_dict = [{'val':abs(specimen['val']),'specimen':specimen['specimen']} for specimen in specimens_dict]
+
+        if self.__selectionEvalMethod == Selection.EvalMethod_Maximum: #normalizing all values to be closest to minimum for MAXIMUM
+            specimens_dict_val_min = min([item['val'] for item in specimens_dict])
+            specimens_dict = [{'val':specimen['val']-specimens_dict_val_min,'specimen':specimen['specimen']} for specimen in specimens_dict]
+            specimens_dict_val_max = max([item['val'] for item in specimens_dict])
+            specimens_dict = [{'val':abs(specimen['val']-specimens_dict_val_max),'specimen':specimen['specimen']} for specimen in specimens_dict]
+            #now the biggest value is 0 and the rest is ascending from it
+
         self.__specimens_selection_amount = 0
         self.__specimens_selection_amount += int(self.__selectionLimitType==Selection.LimitType_Constant)*max(1,min(self.__selectionLimitValue,len(specimens_dict))) #if constant, then min(limit,len_of_species)
         self.__specimens_selection_amount += int(self.__selectionLimitType==Selection.LimitType_Percentage)*max(1,min(int(len(specimens_dict)*(self.__selectionLimitValue/100)),len(specimens_dict))) #if percentage, then min(len_of_species*percentage,len_of_species)
@@ -68,7 +101,6 @@ class Selection():
         specimens_dict_return = sorted(specimens_dict_return, key=lambda x: x['val']) #sorting ascending
         specimens_selected = [item['specimen'] for item in specimens_dict_return] #list of specimens
         return specimens_selected
-
 
 
     def __selection_Top(self,specimens_dict: Collection[Specimen]) -> Collection[Specimen]:
@@ -106,6 +138,12 @@ class Selection():
 
 
 if __name__ == "__main__":
+
+    # SelLimitType = Selection.LimitType_Constant
+    SelLimitType = Selection.LimitType_Percentage
+    SelLimitVal = 18
+    # SelEvalMethod = Selection.EvalMethod_Minimum
+    SelEvalMethod = Selection.EvalMethod_Maximum
     
     print("TEST ________ Selection TOP ________________________________________________")
     SpecimensToBeSelected = []
@@ -115,7 +153,7 @@ if __name__ == "__main__":
     Specimen_Values_First = []
     Specimen_Values_Selected = []
 
-    Sel = Selection(FitnessFunction(),Selection.SelectionType_Top,Selection.LimitType_Constant,10)
+    Sel = Selection(FitnessFunction(),Selection.SelectionType_Top,SelLimitType,SelLimitVal,SelEvalMethod)
     for i in range(SpecimenAmount):
         SpecToAdd = Specimen()
         Specimen_ValMin = min(Specimen_ValMin,SpecToAdd.val)
@@ -129,7 +167,7 @@ if __name__ == "__main__":
         Specimen_Values_Selected += [Spec.val]
 
     print("Added",SpecimenAmount,"Specimens",Specimen_Values_First)
-    print("Selected",len(SpecimensSelected),"Specimens",Specimen_Values_Selected)
+    print("Selected",len(SpecimensSelected),"Specimens",Specimen_Values_Selected,100*len(Specimen_Values_Selected)/len(Specimen_Values_First))
     print()
 
     
@@ -142,7 +180,7 @@ if __name__ == "__main__":
     Specimen_Values_First = []
     Specimen_Values_Selected = []
 
-    Sel = Selection(FitnessFunction(),Selection.SelectionType_Roulette,Selection.LimitType_Constant,10)
+    Sel = Selection(FitnessFunction(),Selection.SelectionType_Roulette,SelLimitType,SelLimitVal,SelEvalMethod)
     for i in range(SpecimenAmount):
         SpecToAdd = Specimen()
         Specimen_ValMin = min(Specimen_ValMin,SpecToAdd.val)
@@ -156,7 +194,7 @@ if __name__ == "__main__":
         Specimen_Values_Selected += [Spec.val]
 
     print("Added",SpecimenAmount,"Specimens",Specimen_Values_First)
-    print("Selected",len(SpecimensSelected),"Specimens",Specimen_Values_Selected)
+    print("Selected",len(SpecimensSelected),"Specimens",Specimen_Values_Selected,100*len(Specimen_Values_Selected)/len(Specimen_Values_First))
     print()
 
     
@@ -169,7 +207,7 @@ if __name__ == "__main__":
     Specimen_Values_First = []
     Specimen_Values_Selected = []
 
-    Sel = Selection(FitnessFunction(),Selection.SelectionType_Tournament,Selection.LimitType_Constant,10)
+    Sel = Selection(FitnessFunction(),Selection.SelectionType_Tournament,SelLimitType,SelLimitVal,SelEvalMethod)
     for i in range(SpecimenAmount):
         SpecToAdd = Specimen()
         Specimen_ValMin = min(Specimen_ValMin,SpecToAdd.val)
@@ -183,5 +221,19 @@ if __name__ == "__main__":
         Specimen_Values_Selected += [Spec.val]
 
     print("Added",SpecimenAmount,"Specimens",Specimen_Values_First)
-    print("Selected",len(SpecimensSelected),"Specimens",Specimen_Values_Selected)
+    print("Selected",len(SpecimensSelected),"Specimens",Specimen_Values_Selected,100*len(Specimen_Values_Selected)/len(Specimen_Values_First))
     print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
