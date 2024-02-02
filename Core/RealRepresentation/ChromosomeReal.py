b9fa69be1578cc055aece9525760cb7c8e86507a
Pwloland <59684145+Pawloland@users.noreply.github.com>
2024-02-02 15:46:21 +0100

    Add helper functions to ChromosomeReal

--- Core/RealRepresentation/ChromosomeReal.py
+++ Core/RealRepresentation/ChromosomeReal.py
@@ -19,3 +19,40 @@ class ChromosomeReal(Chromosome):
 
     def _checkIfNumberIsInDomain(self, chromosome: float):
         return super()._checkIfNumberIsInDomain(chromosome)
+
+    @staticmethod
+    def calculateFunctionsDomainsIntersection(first: Tuple[float, float], second: Tuple[float, float]) -> Tuple[float, float]:
+        """
+        Calculates new chromosome domain from two chromosomes.
+        :param first: Tuple[float, float] ; First chromosome.
+        :param second: Tuple[float, float] ; Second chromosome.
+        :return: Tuple[float, float] ; New chromosome domain from two chromosomes (max(a,b), min(A,B)), where <a,A> and <b,B> are domains for first and second chromosome respectively.
+        :raises ValueError: If domains do not intersect.
+        """
+        a, A = first
+        b, B = second
+        new_range = (max(a, b), min(A, B))
+        if new_range[0] > new_range[1]:
+            raise ValueError("Domains do not intersect")
+
+        return new_range
+
+    @staticmethod
+    def clampNumberToDomain(number: float, domain: Tuple[float, float]) -> float:
+        """
+        Clamps number to the domain. If number is smaller than domain[0], domain[0] is returned. If number is bigger than domain[1], domain[1] is returned.
+        If the number is in the domain, it is returned unchanged.
+        :param number: float Number to clamp.
+        :param domain: Tuple[float, float] Domain to clamp to.
+        :return: float ; Clamped number.
+        """
+        match number:
+            case _ if number < domain[0]:
+                return domain[0]
+            case _ if number > domain[1]:
+                return domain[1]
+            case _:
+                return number
+
+    def __float__(self):
+        return self.getChromosome()
