55164f2634f2f1418bd60e4e6d9acdea2a5ac373
karolgacon22 <karolgacon22@gmail.com>
2024-02-25 11:33:33 +0100

    Change structure of Selection file

--- Selection/Selection.py
+++ Selection/Selection.py
@@ -1,130 +1,217 @@
-from typing import *
-import random
-from Core.FitnessFunction import FitnessFunction
+from typing import Collection
 from Core.Specimen import Specimen
+from Core.FitnessFunction import FitnessFunction
+import random
 
 
 class Selection:
     """
-    Class managing selection of population
+    Class representing the selection process in a genetic algorithm.
+
+    Attributes:
+    - __fitnessFunction: Fitness function for evaluating specimens.
+    - __selectionType: Type of selection strategy.
+    - __selectionLimitType: Type of selection limit (Constant or Percentage).
+    - __selectionLimitValue: Value of the selection limit.
+    - __maximize: Boolean indicating whether to maximize or minimize the fitness function.
+    - __specimens_selection_amount: Number of specimens to be selected.
+
+    Methods:
+    - selection(specimens: Collection[Specimen]) -> Collection[Specimen]:
+        Selects specimens based on the configured selection strategy.
     """
 
-    SelectionType_Top = 1  # Top - we choose n amount of best specimens
-    SelectionType_Roulette = 2  # Roulette - random, where fitness is a weight
-    SelectionType_Tournament = 3  # Tournament - specimens fight each other in brackets until there are more than desired specimens
+    SelectionType_Top = 1
+    SelectionType_Roulette = 2
+    SelectionType_Tournament = 3
+    LimitType_Constant = 1
+    LimitType_Percentage = 2
+    EvalMethod_Minimum = 1
+    EvalMethod_Maximum = 2
 
-    LimitType_Constant = 1  # LimitValue=10 == 10 top osobnikow
-    LimitType_Percentage = 2  # LimitValue=10 == 10% top osobnikow
-
-    EvalMethod_Minimum = 1  # closer to zero the specimen fitness, the better
-    EvalMethod_Maximum = 2  # higher the specimen fitness, the better
-
-    def __init__(self, fitnessFunction: FitnessFunction, selectionType: int, selectionLimitType: str,
+    def __init__(self, fitnessFunction: FitnessFunction, selectionType: int, selectionLimitType: int,
                  selectionLimitValue: int, selectionEvalMethod: int):
         """
-        Constructor for Selection class
+        Initializes the Selection object.
 
-        :param fitnessFunction: Function to evaluate specimen fitness.
-        :type fitnessFunction: FitnessFunction
-
-        :param selectionType: Type of selection (Top,Roulette,Tournament).
-        :type selectionType: int
-
-        :param selectionLimitType: Type of final amount of selected specimens (Constant,Percentage).
-        :type selectionLimitType: int
-
-        :param selectionLimitValue: Value of final amount of selected specimens (Constant,Percentage).
-        :type selectionLimitValue: int
-
-        :param selectionEvalMethod: Method of evaluating specimens fitness (Minimum,Maximum).
-        :type selectionEvalMethod: int
+        :param fitnessFunction: Fitness function for evaluating specimens.
+        :param selectionType: Type of selection strategy.
+        :param selectionLimitType: Type of selection limit (Constant or Percentage).
+        :param selectionLimitValue: Value of the selection limit.
+        :param selectionEvalMethod: Evaluation method (Minimum or Maximum).
         """
         self.__fitnessFunction = fitnessFunction
         self.__selectionType = selectionType
         self.__selectionLimitType = selectionLimitType
         self.__selectionLimitValue = selectionLimitValue
-        self.__maximize = True if selectionEvalMethod == Selection.EvalMethod_Maximum else False
-
+        self.__maximize = selectionEvalMethod == Selection.EvalMethod_Maximum
         self.__specimens_selection_amount = 0
 
     def selection(self, specimens: Collection[Specimen]) -> Collection[Specimen]:
         """
-        Select specimens based on their fitness and selection type.
+        Selects specimens based on the configured selection strategy.
 
-        :param specimens: The specimens to select from.
-        :type specimens: Collection[Specimen]
-        :return: Selected top specimens.
-        :rtype: Collection[Specimen]
+        :param specimens: Collection of specimens to select from.
+
+        :return: Selected specimens based on the configured strategy.
         """
-        # fitness values
+        specimens_copy = list(specimens)
+        self.__specimens_selection_amount = self.calculate_selection_amount(specimens_copy)
 
-        specimensCopy = [specimen for specimen in specimens]
+        selection_strategy = self.get_selection_strategy()
+        specimens_selected = selection_strategy.select(specimens_copy)
 
-        self.__specimens_selection_amount = 0
-        self.__specimens_selection_amount += int(self.__selectionLimitType == Selection.LimitType_Constant) * max(1,
-                                                                                                                  min(self.__selectionLimitValue,
-                                                                                                                      len(specimensCopy)))  # if constant, then min(limit,len_of_species)
-        self.__specimens_selection_amount += int(self.__selectionLimitType == Selection.LimitType_Percentage) * max(1,
-                                                                                                                    min(int(
-                                                                                                                        len(specimensCopy) * (
-                                                                                                                                self.__selectionLimitValue / 100)),
-                                                                                                                        len(specimensCopy)))  # if percentage, then min(len_of_species*percentage,len_of_species)
+        return sorted(specimens_selected, key=lambda x: x.getSpecimenValue())
 
-        self.__specimens_selection_amount = int(self.__specimens_selection_amount)
-        specimens_selected = []
+    def calculate_selection_amount(self, specimens_copy: Collection[Specimen]) -> int:
+        """
+        Calculates the number of specimens to be selected based on the configured limit.
 
+        :param specimens_copy: Copy of the collection of specimens.
+
+        :return: Calculated number of specimens to be selected.
+        """
+        selection_limit = self.__selectionLimitValue
+        if self.__selectionLimitType == Selection.LimitType_Percentage:
+            selection_limit = int(len(specimens_copy) * (selection_limit / 100))
+
+        return max(1, min(selection_limit, len(specimens_copy)))
+
+    def get_selection_strategy(self):
+        """
+        Returns the appropriate selection strategy based on the configured type.
+
+        :return: Instance of the selected selection strategy.
+        """
         if self.__selectionType == Selection.SelectionType_Top:
-            specimens_selected = self.__selection_Top(specimensCopy, self.__maximize)
-
-        if self.__selectionType == Selection.SelectionType_Roulette:
-            specimens_selected = self.__selection_Roulette(specimensCopy, self.__maximize)
-
-        if self.__selectionType == Selection.SelectionType_Tournament:
-            specimens_selected = self.__selection_Tournament(specimensCopy, self.__maximize)
-
-        specimens_selected = sorted(specimens_selected, key=lambda x: x.getSpecimenValue())  # sorting ascending
-        return specimens_selected
-
-    def __selection_Top(self, specimens: Collection[Specimen], maximize: bool) -> Collection[Specimen]:
-        specimens = sorted(specimens, key=lambda x: x.getSpecimenValue(), reverse=maximize)
-
-        return specimens[0:self.__specimens_selection_amount]
-
-    def __selection_Roulette(self, specimens: Collection[Specimen], maximize: bool) -> Collection[Specimen]:
-        values = []
-
-        if maximize:
-            values.extend([specimen.getSpecimenValue() for specimen in specimens])
+            return TopSelection(self.__specimens_selection_amount, self.__maximize)
+        elif self.__selectionType == Selection.SelectionType_Roulette:
+            return RouletteSelection(self.__specimens_selection_amount, self.__maximize)
+        elif self.__selectionType == Selection.SelectionType_Tournament:
+            return TournamentSelection(self.__specimens_selection_amount, self.__maximize)
         else:
-            values.extend([specimen.getSpecimenValue() * -1 for specimen in specimens])
+            raise ValueError("Invalid selection type")
 
-        lowestValue = min(values)
-        currentFitnessSum = sum([value + abs(lowestValue) for value in values])
-        if currentFitnessSum == 0:
-            weights = [1 for _ in range(len(values))]
-        else:
-            weights = [(values[index] + abs(lowestValue)) / currentFitnessSum for index in range(len(values))]
 
-        randomSelectedSpecimens = random.choices(specimens, weights=weights, k=self.__specimens_selection_amount)
+class TopSelection:
+    """
+    Class representing the top selection strategy.
 
-        return randomSelectedSpecimens
+    Attributes:
+    - num_selections: Number of specimens to be selected.
+    - maximize: Boolean indicating whether to maximize or minimize the fitness function.
 
-    def __selection_Tournament(self, specimens: Collection[Specimen], maximize: bool) -> Collection[Specimen]:
-        selectedSpecimens = []
+    Methods:
+    - select(specimens: Collection[Specimen]) -> Collection[Specimen]:
+        Selects specimens based on the top selection strategy.
+    """
 
-        while len(selectedSpecimens) < self.__specimens_selection_amount:
-            copyOfSpecimens = [specimen for specimen in specimens]
-            random.shuffle(copyOfSpecimens)  # random setup of brackets
-            tournamentGroups = [tuple([copyOfSpecimens[ind], copyOfSpecimens[ind + 1], copyOfSpecimens[ind + 2]]) for
-                                ind in range(0, len(copyOfSpecimens), 3) if ind + 2 < len(copyOfSpecimens)]
+    def __init__(self, num_selections: int, maximize: bool):
+        """
+        Initializes the TopSelection object.
 
-            for group in tournamentGroups:
-                if maximize:
-                    bestSpecimen = max(group, key=lambda x: x.getSpecimenValue())
-                else:
-                    bestSpecimen = min(group, key=lambda x: x.getSpecimenValue())
+        :param num_selections: Number of specimens to be selected.
+        :param maximize: Boolean indicating whether to maximize or minimize the fitness function.
+        """
+        self.num_selections = num_selections
+        self.maximize = maximize
 
-                if len(selectedSpecimens) < self.__specimens_selection_amount:
-                    selectedSpecimens.append(bestSpecimen)
+    def select(self, specimens: Collection[Specimen]) -> Collection[Specimen]:
+        """
+        Selects specimens based on the top selection strategy.
 
-        return selectedSpecimens
+        :param specimens: Collection of specimens to select from.
+
+        :return: Selected specimens based on the top selection strategy.
+        """
+        return sorted(specimens, key=lambda x: x.getSpecimenValue(), reverse=self.maximize)[:self.num_selections]
+
+
+class RouletteSelection:
+    """
+    Class representing the roulette wheel selection strategy.
+
+    Attributes:
+    - num_selections: Number of specimens to be selected.
+    - maximize: Boolean indicating whether to maximize or minimize the fitness function.
+
+    Methods:
+    - select(specimens: Collection[Specimen]) -> Collection[Specimen]:
+        Selects specimens based on the roulette wheel selection strategy.
+    """
+
+    def __init__(self, num_selections: int, maximize: bool):
+        """
+        Initializes the RouletteSelection object.
+
+        :param num_selections: Number of specimens to be selected.
+        :param maximize: Boolean indicating whether to maximize or minimize the fitness function.
+        """
+        self.num_selections = num_selections
+        self.maximize = maximize
+
+    def select(self, specimens: Collection[Specimen]) -> Collection[Specimen]:
+        """
+        Selects specimens based on the roulette wheel selection strategy.
+
+        :param specimens: Collection of specimens to select from.
+
+        :return: Selected specimens based on the roulette wheel selection strategy.
+        """
+        values = [specimen.getSpecimenValue() if self.maximize else -specimen.getSpecimenValue() for specimen in
+                  specimens]
+        lowest_value = min(values)
+        current_fitness_sum = sum([value + abs(lowest_value) for value in values])
+
+
+        weights = [(value + abs(lowest_value)) / current_fitness_sum if current_fitness_sum != 0 else 1 for value in
+                   values]
+
+        self.num_selections =int(self.num_selections)
+        return random.choices(specimens, weights=weights, k=self.num_selections)
+
+
+class TournamentSelection:
+    """
+    Class representing the tournament selection strategy.
+
+    Attributes:
+    - num_selections: Number of specimens to be selected.
+    - maximize: Boolean indicating whether to maximize or minimize the fitness function.
+
+    Methods:
+    - select(specimens: Collection[Specimen]) -> Collection[Specimen]:
+        Selects specimens based on the tournament selection strategy.
+    """
+
+    def __init__(self, num_selections: int, maximize: bool):
+        """
+        Initializes the TournamentSelection object.
+
+        :param num_selections: Number of specimens to be selected.
+        :param maximize: Boolean indicating whether to maximize or minimize the fitness function.
+        """
+        self.num_selections = num_selections
+        self.maximize = maximize
+
+    def select(self, specimens: Collection[Specimen]) -> Collection[Specimen]:
+        """
+        Selects specimens based on the tournament selection strategy.
+
+        :param specimens: Collection of specimens to select from.
+
+        :return: Selected specimens based on the tournament selection strategy.
+        """
+        selected_specimens = []
+
+        while len(selected_specimens) < self.num_selections:
+            copy_of_specimens = list(specimens)
+            random.shuffle(copy_of_specimens)
+            tournament_groups = [tuple(copy_of_specimens[ind:ind+3]) for ind in range(0, len(copy_of_specimens), 3) if ind + 2 < len(copy_of_specimens)]
+
+            for group in tournament_groups:
+                best_specimen = max(group, key=lambda x: x.getSpecimenValue()) if self.maximize else min(group, key=lambda x: x.getSpecimenValue())
+                if len(selected_specimens) < self.num_selections:
+                    selected_specimens.append(best_specimen)
+
+        return selected_specimens
